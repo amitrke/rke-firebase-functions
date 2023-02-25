f57f492a0af9c5e63c3dea5ff07e58e8b1c4f9a3
Amit Kumar <amitrke@gmail.com>
2023-02-25 14:09:53 -0800

    Updated weather function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,11 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import {listAndInsertFiles} from "./files";
 import {updateWeatherUtil} from "./weather";
+
 admin.initializeApp();
 
-export const helloWorld = functions
-  .region("us-east1")
-  .https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-  }
-  );
-
 export const updateWeather = functions
+  .region("us-east1")
   .pubsub.schedule("every 120 minutes").onRun(async () => {
     functions.logger.info(
       "Function updateWhether triggered", {structuredData: true});
--- functions/src/weather.ts
+++ functions/src/weather.ts
@@ -1,17 +1,16 @@
 import {Weather} from "./model/types";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import fetch from "node-fetch";
 
 export const updateWeatherUtil = async () => {
-  const WEATHER_APPID = functions.config().config.WEATHER_APPID;
+  const WEATHER_APPID = functions.config().config.pass;
   const response = await fetch(
     "http://api.openweathermap.org/data/2.5/onecall?lat=29.8667"+
         `&lon=77.8833&appid=${WEATHER_APPID}`+
         "&units=metric&exclude=minutely,hourly"
   );
-  const body: Weather = await response.json();
-  const db = admin.database();
-  const ref = db.ref("weather").child("roorkee-in");
-  await ref.set(body);
-  console.log(body);
+  const body: Weather = <Weather> await response.json();
+  const weatherCollection = admin.firestore().collection("weather");
+  await weatherCollection.doc("roorkee-in").set(body);
 };
