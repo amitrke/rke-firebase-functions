96c3bdeda97a5f6e3d92b65d2648dab0a3d6709a
Amit Kumar <amitrke@gmail.com>
2025-08-04 12:43:01 -0600

    Disable no-explicit-any rule in ESLint configuration for TypeScript

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -32,5 +32,6 @@ module.exports = {
     "import/no-unresolved": 0,
     "indent": ["error", 2],
     "max-len": ["error", {"code": 120}],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
