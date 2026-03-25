b9b326ff21d19ee4e81c6d55b05b8984bd75c136
Amit Kumar <amitrke@gmail.com>
2026-03-25 11:49:48 -0600

    Refactor code structure for improved readability and maintainability

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./setupTests.js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        module: 'commonjs',
+        esModuleInterop: true,
+        resolveJsonModule: true,
+        types: ['jest', 'node'],
+      },
+    },
+  },
 };
